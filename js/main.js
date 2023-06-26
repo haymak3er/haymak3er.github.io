930159a656d502d6cefa4df37ec1de500a85d6e8
BuildTools <iexistanddontknowabetteremail@gmail.com>
2023-06-26 14:02:10 -0400

    improvements

---
+++ js/main.js
@@ -0,0 +1,20 @@
+ttl = document.title;
+idx = 0;
+
+$(document).ready(function() {
+    $('#githubplug-label').click(function() { document.location = 'https://github.com/haymak3er/' });
+    $('#projects-label').click(function() { document.location.pathname = `projects.html` });
+})
+
+const interval = setInterval(function() {
+    nttl = "";
+    for(var i = 0; i < ttl.length; i++) {
+        if(i == idx) nttl += "_";
+        else nttl += ttl[i];
+    }
+
+    if(idx >= ttl.length) idx = 0;
+    else idx += 1;
+
+    document.title = `☫ ${nttl}`
+}, 250);
