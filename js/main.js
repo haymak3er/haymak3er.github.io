68bd23491185d0a5a39dda0e2151a3818f9084ad
BuildTools <iexistanddontknowabetteremail@gmail.com>
2023-07-11 08:24:02 -0400

    imprvmtz

--- js/main.js
+++ js/main.js
@@ -1,21 +1,7 @@
-ttl = document.title;
-idx = 0;
+document.title = `☫ ${document.title}`;
 
 $(document).ready(function() {
     $('#githubplug-label').click(function() { document.location = 'https://github.com/haymak3er/' });
     $('#projects-label').click(function() { document.location.pathname = `projects.html` });
-    $('#email-label').click(function() { alert(`mailto:${$('#email-label').val()}`); document.location = `mailto:${$('#email-label').val()}` });
+    $('#email-label').click(function() { document.location = `mailto:${$('#email-label').html()}` });
 })
-
-const interval = setInterval(function() {
-    nttl = "";
-    for(var i = 0; i < ttl.length; i++) {
-        if(i == idx) nttl += "_";
-        else nttl += ttl[i];
-    }
-
-    if(idx >= ttl.length) idx = 0;
-    else idx += 1;
-
-    document.title = `☫ ${nttl}`
-}, 250);
