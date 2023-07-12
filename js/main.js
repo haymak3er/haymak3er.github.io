6ca2caf2c5b441ff724d3915b2bcfbbf9a5d6cd0
BuildTools <iexistanddontknowabetteremail@gmail.com>
2023-07-11 21:38:29 -0400

    relocated and created

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ document.title = `☫ ${document.title}`;
 
 $(document).ready(function() {
     $('#githubplug-label').click(function() { document.location = 'https://github.com/haymak3er/' });
-    $('#pgp-label').click(function() { document.location.pathname = `/res/pgp/public.pgp` });
+    $('#pgp-label').click(function() { document.location.pathname = `/pgp/public.pgp` });
     $('#email-label').click(function() { document.location = `mailto:${$('#email-label').html()}` });
 })
