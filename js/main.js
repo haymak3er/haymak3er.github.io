57d0c831acf47c432077d51920532ad843c39507
BuildTools <iexistanddontknowabetteremail@gmail.com>
2023-07-12 07:44:16 -0400

    added pgp message

--- js/main.js
+++ js/main.js
@@ -3,5 +3,7 @@ document.title = `☫ ${document.title}`;
 $(document).ready(function() {
     $('#githubplug-label').click(function() { document.location = 'https://github.com/haymak3er/' });
     $('#pgp-label').click(function() { document.location.pathname = `/pgp/public.pgp` });
+    $('#messages-label').click(function() { document.location.pathname = `/pgp/messages/index.html` });
     $('#email-label').click(function() { document.location = `mailto:${$('#email-label').html()}` });
+    $('#freesha666y').click(function() { document.location = 'https://soundcloud.com/shane-behringer' });
 })
