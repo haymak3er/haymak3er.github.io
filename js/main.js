35e2141b1209c610fa452bfd401ee8ef9710f24e
BuildTools <iexistanddontknowabetteremail@gmail.com>
2023-07-11 20:24:05 -0400

    removed projects added pgp stuff

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ document.title = `☫ ${document.title}`;
 
 $(document).ready(function() {
     $('#githubplug-label').click(function() { document.location = 'https://github.com/haymak3er/' });
-    $('#projects-label').click(function() { document.location.pathname = `projects.html` });
+    $('#pgp-label').click(function() { document.location.pathname = `/res/pgp/public.pgp` });
     $('#email-label').click(function() { document.location = `mailto:${$('#email-label').html()}` });
 })
